9737892d073e1d6c4f2687e6299bf3d64c5b351c
Muwa1011 <46922312+Muwa1011@users.noreply.github.com>
2022-06-01 15:30:34 +0200

    ten

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ let id = 0;
 function Creature(div, size, speed, destination, destinationReachedLeft, destinationReachedTop) {
     this.id = id++;
     this.div = div;
+    this.div.classList.add("creature")
+    box.append(this.div)
     this.size = size;
     this.speed = speed;
     this.destination = destination;
     this.destinationReachedLeft = destinationReachedLeft;
     this.destinationReachedTop = destinationReachedTop;
+    //??? this.move = move()??
+    this.move()
+
+
     console.log(this)
 }
 
@@ -18,33 +24,29 @@ function getDestination() {
     return [randomPositionLeft, randomPositionTop]
 }
 
-let destination = getDestination();
-let destinationReachedLeft = false;
-let destinationReachedTop = false;
-
-console.log(destination)
 
 Creature.prototype.move = function () {
     //Left
-    if(this.div.getBoundingClientRect().left < this.destination[0]){
+    if (this.div.getBoundingClientRect().left < this.destination[0]) {
         this.div.style.left = this.div.getBoundingClientRect().left + 1 + 'px';
-    } else if(this.div.getBoundingClientRect().left > this.destination[0]){
+    } else if (this.div.getBoundingClientRect().left > this.destination[0]) {
         this.div.style.left = this.div.getBoundingClientRect().left - 1 + 'px';
     } else {
         this.destinationReachedLeft = true;
     }
 
     //Top
-    if(this.div.getBoundingClientRect().top < this.destination[1]){
+    if (this.div.getBoundingClientRect().top < this.destination[1]) {
         this.div.style.top = this.div.getBoundingClientRect().top + 1 + 'px';
-    } else if(this.div.getBoundingClientRect().top > this.destination[1]){
+    } else if (this.div.getBoundingClientRect().top > this.destination[1]) {
         this.div.style.top = this.div.getBoundingClientRect().top - 1 + 'px';
     } else {
         this.destinationReachedTop = true;
     }
 
-    if(this.destinationReachedLeft === true && this.destinationReachedTop === true){
-        this. destination = getDestination()
+    //Reached
+    if (this.destinationReachedLeft === true && this.destinationReachedTop === true) {
+        this.destination = getDestination()
         this.destinationReachedLeft = false;
         this.destinationReachedTop = false;
     }
@@ -53,17 +55,33 @@ Creature.prototype.move = function () {
 setInterval(function () {
     creature.move()
     creature2.move()
+    creature3.move()
+    creature4.move()
+    creature5.move()
+    creature6.move()
+    creature7.move()
+    creature8.move()
+    creature9.move()
+    creature10.move()
 }, 1);
 
-const creatureDiv = document.createElement("div");
-const creatureDiv2 = document.createElement("div");
-creatureDiv.classList.add("creature")
-creatureDiv2.classList.add("creature")
-box.append(creatureDiv)
-box.append(creatureDiv2)
+const creature = new Creature(document.createElement("div"), 1, 1, getDestination(), false, false);
+const creature2 = new Creature(document.createElement("div"), 1, 1, getDestination(), false, false);
+const creature3 = new Creature(document.createElement("div"), 1, 1, getDestination(), false, false);
+const creature4 = new Creature(document.createElement("div"), 1, 1, getDestination(), false, false);
+const creature5 = new Creature(document.createElement("div"), 1, 1, getDestination(), false, false);
+const creature6 = new Creature(document.createElement("div"), 1, 1, getDestination(), false, false);
+const creature7 = new Creature(document.createElement("div"), 1, 1, getDestination(), false, false);
+const creature8 = new Creature(document.createElement("div"), 1, 1, getDestination(), false, false);
+const creature9 = new Creature(document.createElement("div"), 1, 1, getDestination(), false, false);
+const creature10 = new Creature(document.createElement("div"), 1, 1, getDestination(), false, false);
 
-const creature = new Creature(creatureDiv, 1, 1, getDestination(), false, false);
-const creature2 = new Creature(creatureDiv2, 1, 1, getDestination(), false, false);
 
-creature.move()
-creature2.move()
+function createCreatures(amount) {
+    for (let i = 0; i < amount; i++) {
+        new Creature(document.createElement("div"), 1, 1, getDestination(), false, false);
+
+    }
+}
+
+createCreatures(100)
