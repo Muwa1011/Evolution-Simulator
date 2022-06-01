a45fe5bb1d86ed8e0ec2e234eccca45614423b6e
Muwa1011 <46922312+Muwa1011@users.noreply.github.com>
2022-06-01 15:13:18 +0200

    two creatures async

--- app.js
+++ app.js
@@ -1,35 +1,20 @@
 const box = document.querySelector(".box")
 let id = 0;
 
-/* 
-function createCreature() {
-    const creature = document.createElement("div")
-    creature.speed = 1;
-    creature.moving = true;
-    creature.move = function () {
-        while (creature.moving) {
-
-        }
-        creature.style.position = 'absolute';
-        creature.style.top = Math.floor(Math.random() * 90 + 5) + '%';
-        creature.style.left = Math.floor(Math.random() * 90 + 5) + '%';
-    }
-    creature.classList.add("creature")
-    box.append(creature)
-    return creature;
-} */
-
-function Creature(div, size, speed) {
+function Creature(div, size, speed, destination, destinationReachedLeft, destinationReachedTop) {
     this.id = id++;
     this.div = div;
     this.size = size;
     this.speed = speed;
+    this.destination = destination;
+    this.destinationReachedLeft = destinationReachedLeft;
+    this.destinationReachedTop = destinationReachedTop;
     console.log(this)
 }
 
 function getDestination() {
-    const randomPositionLeft = Math.floor(Math.random() * 200) + 150
-    const randomPositionTop = Math.floor(Math.random() * 200)
+    const randomPositionLeft = Math.floor(Math.random() * 1500) + 150
+    const randomPositionTop = Math.floor(Math.random() * 600) + 200
     return [randomPositionLeft, randomPositionTop]
 }
 
@@ -40,50 +25,45 @@ let destinationReachedTop = false;
 console.log(destination)
 
 Creature.prototype.move = function () {
-
-    //this.div.style.top = this.div.getBoundingClientRect().top + 1 + 'px';
     //Left
-    if(this.div.getBoundingClientRect().left < destination[0]){
+    if(this.div.getBoundingClientRect().left < this.destination[0]){
         this.div.style.left = this.div.getBoundingClientRect().left + 1 + 'px';
-        console.log("move right")
-    } else if(this.div.getBoundingClientRect().left > destination[0]){
+    } else if(this.div.getBoundingClientRect().left > this.destination[0]){
         this.div.style.left = this.div.getBoundingClientRect().left - 1 + 'px';
-        console.log("move left")
     } else {
-        console.log("got there")
-        destinationReachedLeft = true;
+        this.destinationReachedLeft = true;
     }
 
     //Top
-    if(this.div.getBoundingClientRect().top < destination[1]){
+    if(this.div.getBoundingClientRect().top < this.destination[1]){
         this.div.style.top = this.div.getBoundingClientRect().top + 1 + 'px';
-        console.log("move right")
-    } else if(this.div.getBoundingClientRect().top > destination[1]){
+    } else if(this.div.getBoundingClientRect().top > this.destination[1]){
         this.div.style.top = this.div.getBoundingClientRect().top - 1 + 'px';
-        console.log("move left")
     } else {
-        console.log("got there")
-        destinationReachedtop = true;
+        this.destinationReachedTop = true;
     }
 
-    if(destinationReachedLeft === true && destinationReachedtop === true){
-        destination = getDestination()
-        destinationReachedLeft = false;
-        destinationReachedtop = false;
+    if(this.destinationReachedLeft === true && this.destinationReachedTop === true){
+        this. destination = getDestination()
+        this.destinationReachedLeft = false;
+        this.destinationReachedTop = false;
     }
-
-
-
 }
 
 setInterval(function () {
     creature.move()
+    creature2.move()
 }, 1);
 
 const creatureDiv = document.createElement("div");
+const creatureDiv2 = document.createElement("div");
 creatureDiv.classList.add("creature")
+creatureDiv2.classList.add("creature")
 box.append(creatureDiv)
+box.append(creatureDiv2)
 
-const creature = new Creature(creatureDiv, 1, 1);
+const creature = new Creature(creatureDiv, 1, 1, getDestination(), false, false);
+const creature2 = new Creature(creatureDiv2, 1, 1, getDestination(), false, false);
 
 creature.move()
+creature2.move()
