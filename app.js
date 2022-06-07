df2b43cb7acb0a84bf5ff65c09e77613dd55d5be
Muwa1011 <46922312+Muwa1011@users.noreply.github.com>
2022-06-07 16:36:30 +0200

    removed Splice Issue

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const box = document.querySelector(".box")
 const populationText = document.querySelector("#population")
+const restartButton = document.querySelector("#resetButton")
+restartButton.addEventListener('click', reset)
 let id = 0;
 
 let creatureArray = [];
@@ -22,13 +24,26 @@ function Creature(div, size, speed, destination, destinationReachedLeft, destina
     this.div.innerText = id
 }
 
+function reset(creatures, food){
+    // Remove Creatures
+    for(let i = creatureArray.length; i > 0; i--){
+        creatureArray[i - 1].div.remove()
+        creatureArray.splice(i - 1, 1)
+    }
+    // Remove Food
+    // restart Intervall Functions
+    // create Creatures
+    // generate Food
+
+}
+
 function getDestination() {
     const randomPositionLeft = Math.floor(Math.random() * 1509) + 199
     const randomPositionTop = Math.floor(Math.random() * 600) + 150
     return [randomPositionLeft, randomPositionTop]
 }
 
-Creature.prototype.goBack = function (){
+Creature.prototype.goBack = function () {
     const destination = this.destination;
 
     const distanceTop = this.div.getBoundingClientRect().top - 130;
@@ -37,34 +52,34 @@ Creature.prototype.goBack = function (){
     const distanceRight = 1509 + 199 - this.div.getBoundingClientRect().left;
 
     let shortestWay = distanceTop;
-    
-    if(distanceBottom < shortestWay) {
+
+    if (distanceBottom < shortestWay) {
         shortestWay = distanceBottom
     }
-    if(distanceLeft < shortestWay){
+    if (distanceLeft < shortestWay) {
         shortestWay = distanceLeft
     }
-    if(distanceRight < shortestWay){
+    if (distanceRight < shortestWay) {
         shortestWay = distanceRight
     }
 
-    if(shortestWay === distanceTop){
-        destination[0] =  this.div.getBoundingClientRect().left;
+    if (shortestWay === distanceTop) {
+        destination[0] = this.div.getBoundingClientRect().left;
         destination[1] = 130;
     }
-    if(shortestWay === distanceBottom){
+    if (shortestWay === distanceBottom) {
         destination[0] = this.div.getBoundingClientRect().left
         destination[1] = 770
     }
-    if(shortestWay === distanceLeft){
+    if (shortestWay === distanceLeft) {
         destination[0] = 199;
-        destination[1] =  this.div.getBoundingClientRect().top;
+        destination[1] = this.div.getBoundingClientRect().top;
     }
-    if(shortestWay === distanceRight){
+    if (shortestWay === distanceRight) {
         destination[0] = 1708;
-        destination[1] =  this.div.getBoundingClientRect().top;
+        destination[1] = this.div.getBoundingClientRect().top;
     }
-        return destination
+    return destination
 }
 
 Creature.prototype.move = function () {
@@ -94,9 +109,9 @@ Creature.prototype.move = function () {
             this.destinationReachedTop = false;
         }
     } else {
-        
+
         this.destinationReachedLeft = false;
-        destinationReachedTop = false;
+        this.destinationReachedTop = false;
         //Left
         if (this.div.getBoundingClientRect().left < this.destination[0]) {
             this.div.style.left = this.div.getBoundingClientRect().left + 1 + 'px';
@@ -132,11 +147,11 @@ function createCreatures(amount) {
     }
 }
 
-function createCreatureOne(parent){
+function createCreatureOne(parent) {
     const c = new Creature(document.createElement("div"), 1, 1, getDestination(), false, false, 0, false, false);
     creatureArray.push(c)
-    c.div.style.top = creatureArray[creatureArray.indexOf(parent)].div.getBoundingClientRect().top
-    c.div.style.left = creatureArray[creatureArray.indexOf(parent)].div.getBoundingClientRect().left
+    c.div.style.top = creatureArray[creatureArray.indexOf(parent)].div.getBoundingClientRect().top + 25 + 'px';
+    c.div.style.left = creatureArray[creatureArray.indexOf(parent)].div.getBoundingClientRect().left + 25 + 'px';
     console.log("c" + c.id)
     console.log("parent" + parent.id)
 
@@ -154,52 +169,59 @@ function generateFood(amount) {
     }
 }
 
-function removeFood(){
-    for(foodSingle of food){
+function removeFood() {
+    for (foodSingle of food) {
         foodSingle.remove()
     }
 }
 
 createCreatures(10)
-generateFood(40)
+generateFood(33)
 
 let generation = 0;
 let population = 0;
 
-setInterval(function (){
-    for(creatureS of creatureArray){
-        if(!creatureS.eaten){
-            creatureS.div.remove()
-            creatureArray.splice(creatureArray.indexOf(creatureS), 1)
+setInterval(function () {
+    for(let i = creatureArray.length; i > 0; i--){
+        if(!creatureArray[i - 1].eaten){
+            creatureArray[i - 1].div.remove();
+            creatureArray.splice(i - 1, 1);
+        }
+    }
+    for (creatureS of creatureArray) {
+        if (!creatureS.eaten) {
+            //TODO if creature is removed the next creature will be skiped (?) 
+            // solution traverse backwards
+            /* creatureS.div.remove()
+            creatureArray.splice(creatureArray.indexOf(creatureS), 1) */
         } else {
             creatureS.destination = getDestination()
             creatureS.eaten = false;
             creatureS.amountEaten = 0;
-            creatureS.div.style.background = "black";
-            if(creatureS.replicate){
-               // createCreatures(1);
+            creatureS.div.style.boxShadow  = "0px 0px 5px 0px rgb(75, 75, 75)";
+            if (creatureS.replicate) {
                 createCreatureOne(creatureS)
                 creatureS.replicate = false;
             }
         }
     }
     removeFood();
-    generateFood(40)
+    generateFood(33)
     let population = 0;
-    for(creatureS of creatureArray){
+    for (creatureS of creatureArray) {
         population++;
     }
     generation++;
     populationText.innerText = `Population: ${population} / Gen: ${generation}`
     console.log(creatureArray)
-},40000)
+}, 40000)
 
 setInterval(function () {
     for (creatureS of creatureArray) {
         creatureS.move()
         creatureS.eat()
     }
-}, 1);
+}, 2);
 
 
 Creature.prototype.eat = function () {
@@ -207,19 +229,20 @@ Creature.prototype.eat = function () {
         if (this.amountEaten < 2) {
             const topDifference = this.div.getBoundingClientRect().top - foodSingle.getBoundingClientRect().top
             const leftDifference = this.div.getBoundingClientRect().left - foodSingle.getBoundingClientRect().left
-            if(((topDifference < 10 && topDifference > 0) || (topDifference < 0 && topDifference > -24)) && ((leftDifference < 10 && leftDifference > 0) || (leftDifference < 0 && leftDifference > -24))){
+            if (((topDifference < 10 && topDifference > 0) || (topDifference < 0 && topDifference > -24)) &&
+                ((leftDifference < 10 && leftDifference > 0) || (leftDifference < 0 && leftDifference > -24))) {
                 this.amountEaten++;
                 this.eaten = true;
-                this.div.style.background = "green";
-                this.destination = this.goBack()
+                this.div.style.boxShadow  = "0px 0px 5px 0px rgb(0, 200, 0)";
                 foodSingle.remove()
-                if(this.amountEaten === 2){
-                    this.div.style.background = "purple";
+                if (this.amountEaten === 2) {
+                    this.div.style.boxShadow  = "0px 0px 5px 0px rgb(144, 0, 255)";
                     this.replicate = true;
+                    this.destination = this.goBack()
                 }
                 //console.log(this)
             }
-            
+
         }
 
     }
